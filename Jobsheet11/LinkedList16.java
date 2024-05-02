1cc5dac7a7f27e4d7a20893e75bed0e3189bfe0d
IzzatirRofiah <144086912+IzzatirRofiah@users.noreply.github.com>
2024-05-02 13:28:18 +0700

    Add files via upload

--- Jobsheet11/LinkedList16.java
+++ Jobsheet11/LinkedList16.java
@@ -74,4 +74,65 @@ public class LinkedList16 {
             }
         }
     }
+    public int getData(int index) {
+        Node16 currentNode = head;
+
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode.data;
+    }
+    public int indexxOf(int key) {
+        Node16 currentNode = head;
+        int index = 0;
+
+        while(currentNode != null && currentNode.data != key) {
+            currentNode = currentNode.next;
+            index++;
+        }
+        if (currentNode == null) {
+            return -1;
+        } else {
+            return index;
+        }
+    }
+    public void removeFirst() {
+        if(!isEmpty()) {
+            head = head.next;
+        } else {
+            System.out.println("Linked list kosong");
+        }
+    }
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked list kosong");
+        }else if (head.next == null) {
+            head = null;
+        } else {
+            Node16 currentNode = head;
+            while (currentNode.next != null) {
+                if (currentNode.next.next == null) {
+                    currentNode.next = null;
+                    break;
+                }
+                currentNode = currentNode.next;
+            }
+        }
+    }
+    public void remove(int key) {
+        if (isEmpty()) {
+            System.out.println("Linked list kosong");
+        } else if (head.data == key) {
+            removeFirst();
+        } else {
+            Node16 currentNode = head;
+            while (currentNode.next != null) {
+                if (currentNode.next.data == key) {
+                    currentNode.next = currentNode.next.next;
+                    break;
+                }
+                currentNode = currentNode.next;
+            }
+        }
+    }
 }
--- Jobsheet11/SLLMain16.java
+++ Jobsheet11/SLLMain16.java
@@ -10,5 +10,14 @@ public class SLLMain16 {
         myLinkedList16.print();
         myLinkedList16.insertAfter(700, 300);
         myLinkedList16.print();
+
+        System.out.println("Data pada index ke-1: " + myLinkedList16.getData(1));
+        System.out.println("Data 300 berada pada index ke: " + myLinkedList16.indexxOf(300));
+        myLinkedList16.remove(300);
+        myLinkedList16.print();
+        myLinkedList16.removeFirst();
+        myLinkedList16.print();
+        myLinkedList16.removeLast();
+        myLinkedList16.print();
     }
 }
